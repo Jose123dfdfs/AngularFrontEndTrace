0977ee656c25971afe6dc9a0707a3fb0431b8f73
Jose Trejo <JTrejo@pronaca.com>
2020-08-19 18:38:56 -0500

    Subida frontend Trace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiTestService} from '../services/api-test.service';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test-trace';
+
+  constructor(private apiTestService: ApiTestService) {}
+
+  createUser() {
+    const user = new User();
+    user.firstName = 'José';
+    user.lastName = 'Trejo';
+    this.apiTestService.createUser(user).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
---
+++ src/services/api-test.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiTestService {
+
+apiURL: string = 'http://localhost:8080/api';
+
+  constructor(private httpClient: HttpClient) { }
+
+public createUser(user: User){
+  return this.httpClient.post(`${this.apiURL}/createUser/`, user);
+
+ }
+
+}
